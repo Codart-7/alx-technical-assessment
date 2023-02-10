87f2edd9926763fa66f0fa5360b411061f4b6bef
Codart-7 <iconmoa@gmail.com>
2023-02-10 15:04:10 +0100

    Problem 1

--- problem_1/README.md
+++ problem_1/README.md
@@ -24,3 +24,10 @@ Output: 9 -> for the sequence: A, A, A, Ctrl-A, Ctrl-C, Ctrl-V, Ctrl-V
 - Answers will be in the range of 32-bit signed integer.
 
 ## Solution Explanation
+`Ctrl-A`, `Ctrl-C` and `Ctrl-V` have to be consecutive for a meaninful result. That takes 3 key strokes.
+For any `n` equal to or below 5, only `A`s make sense.
+At `n` = 6, we observe a behaviour: 6 `A`s give the same result as 3 `A`s and `Ctrl-A`, `Ctrl-C` and `Ctrl-V`.
+The pattern is repeatable and the goal is to use this to increase the buffer.
+
+For example, after `n` = 6, it is safe to continue using `Ctrl-V`
+But at `n` = 10, the result is 18, which is the same as replacing the last 3 elements of sequence with `Ctrl-A`, `Ctrl-C` and `Ctrl-V`. This is used to increase the buffer.
--- problem_1/problem_1.py
+++ problem_1/problem_1.py
@@ -1,3 +1,50 @@
 #!/usr/bin/env python3
+"""find out the maximum numbers of 'A' you can print on screen
 """
-"""
+from typing import List
+
+
+def list_append(data: List, value, instruction: str):
+    if instruction == "append":
+        data.append(value)
+    elif instruction == "replace":
+        data[-1] = value[1]
+        data[-2] = value[0]
+        data.append(value[2])
+
+
+def max_output(n: int):
+    """hello"""
+    char_List = ["A", "Ctrl-A", "Ctrl-C", "Ctrl-V"]
+    sequence_List = list()
+    v_count = 0
+    result_count = 0
+    copy_count = 0
+
+    for i in range(n):
+        if i < 5:
+            result_count += 1
+            list_append(sequence_List, char_List[0], "append")
+
+        elif i == 5:
+            list_append(sequence_List, char_List[1:], "replace")
+            result_count -= 2
+            copy_count = result_count
+            result_count += copy_count
+
+        if i > 5 and v_count < 3:
+            result_count += copy_count
+            list_append(sequence_List, char_List[3], "append")
+            v_count += 1
+
+        elif i > 5 and v_count == 3:
+            v_count = 0
+            list_append(sequence_List, char_List[1:], "replace")
+            result_count -= (copy_count * 2)
+            copy_count = result_count
+            result_count += copy_count
+    
+    print(result_count, "-> for the sequence: {}".format(sequence_List))
+            
+
+max_output(14)
